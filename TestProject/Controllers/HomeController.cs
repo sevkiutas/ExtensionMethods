eaa742916739ab3ec561ce3387327080d50820ab
Şevki Utaş <sutas2@ford.com.tr>
2024-03-23 12:55:47 +0300

    Add project files.

---
+++ TestProject/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+using TestProject.Extensions;
+
+
+namespace TestProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            DateTime paymentDate = new DateTime(2024, 04, 23);
+
+            List<DateTime> holidays = new List<DateTime>
+            {
+                new DateTime(2024, 04, 23), // Ulusal Egemenlik ve Çocuk Bayramı
+                new DateTime(2024, 05, 01), // İşçi Bayramı
+                new DateTime(2024, 05, 19), // 19 Mayıs Gençlik ve Spor Bayramı
+                new DateTime(2024, 10, 29), // Cumhuriyet Bayramı Bayramı
+            };
+            DateTime lastPaymentDate = paymentDate.CalculateLastPaymentDate(holidays);
+
+            return View();
+        }
+    }
+}
---
+++ TestProject/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace TestProject.Extensions
+{
+    public static class DateTimeExtensions
+    {
+
+        public static DateTime CalculateLastPaymentDate(this DateTime date, List<DateTime> holidays)
+        {
+            while (IsWeekendOrHoliday(date, holidays))
+            {
+
+                date = date.AddDays(1);
+
+            }
+            return date;
+        }
+        private static bool IsWeekendOrHoliday(DateTime date, List<DateTime> holidays)
+        {
+            return date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday || holidays?.Contains(date) == true;
+        }
+    }
+}
